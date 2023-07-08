3c7792a321db14ac9f6039d763f8bdab90c65589
MR.FIRST <128628624+Uncontested26@users.noreply.github.com>
2023-07-08 20:52:06 +0300

    Spring mvc-CRUD

---
+++ src/main/java/springCrud/model/User.java
@@ -0,0 +1,72 @@
+package springCrud.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "profession")
+    private String profession;
+
+    public User() {
+    }
+
+    public User(int id, String name, int age, String profession) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", profession='" + profession + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/springCrud/service/UserService.java
@@ -0,0 +1,17 @@
+package springCrud.service;
+
+import springCrud.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public void add(User user);
+
+    void updateUser( int id, User user);
+
+    public void deleteUser(int id);
+
+    public User getUserById(int id);
+
+    public List<User> getAllUsers();
+}
