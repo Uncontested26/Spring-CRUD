6ea77c400437cd1a0e1eea5df06761b0f69d0c00
MR.FIRST <128628624+Uncontested26@users.noreply.github.com>
2023-07-11 22:54:56 +0300

    Update UserServiceImpl.java

--- src/main/java/springCrud/service/UserServiceImpl.java
+++ src/main/java/springCrud/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package springCrud.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;\
+import org.springframework.transaction.annotation.Transactional;
 import springCrud.dao.UserDao;
 import springCrud.dao.UserDaoImpl;
 import springCrud.model.User;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
 
 @Service
